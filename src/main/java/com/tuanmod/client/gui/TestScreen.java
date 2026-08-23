b5fa64ac8fb8772f7cfb9fbefdc5e6cf5dc1b642
tuanq2893-alt <tuanq2893@gmail.com>
2026-08-23 11:15:36 +0700

    Create TestScreen.java

---
+++ src/main/java/com/tuanmod/client/gui/TestScreen.java
@@ -0,0 +1,18 @@
+package com.tuanmod.client.gui;
+
+import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.text.Text;
+
+public class TestScreen extends Screen {
+    public TestScreen() {
+        super(Text.literal("Mod GUI"));
+    }
+
+    @Override
+    public void render(DrawContext context, int mouseX, int mouseY, float delta) {
+        this.renderBackground(context);
+        super.render(context, mouseX, mouseY, delta);
+        context.drawCenteredTextWithShadow(this.textRenderer, "GIAO DIỆN MOD ĐÃ MỞ!", this.width / 2, this.height / 2, 0xFFFFFF);
+    }
+}
